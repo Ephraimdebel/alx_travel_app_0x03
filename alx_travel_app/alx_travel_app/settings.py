54695a130f583aa72c4b12ea1f5c0cd6512afae6
Ephraim Debel <ephraimdebel@gmail.com>
2025-11-01 21:29:49 +0300

    fix setting.py location update

---
+++ alx_travel_app/alx_travel_app/settings.py
@@ -0,0 +1,74 @@
+import environ
+import os
+from pathlib import Path
+from .celery import app as celery_app
+__all__ = ('celery_app',)
+
+env = environ.Env()
+environ.Env.read_env()
+
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = env("SECRET_KEY", default="unsafe-secret")
+DEBUG = env.bool("DEBUG", default=True)
+
+ALLOWED_HOSTS = ["*"]
+
+# Installed apps
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "corsheaders",
+    "listings",
+    "drf_yasg",
+]
+
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "alx_travel_app.urls"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+    }
+}
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+# ---------------- CELERY CONFIG ----------------
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost//'
+CELERY_RESULT_BACKEND = 'rpc://'
+
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+# ---------------- EMAIL CONFIG -----------------
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'   # replace or use .env
+EMAIL_HOST_PASSWORD = 'your_password'      # replace or use .env
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
